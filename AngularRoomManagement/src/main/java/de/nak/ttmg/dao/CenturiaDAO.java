33902c7c124a2f428cb2a5dd9a40b9409a67c6c1
Felix Behrendt <felixbehrendt@me.com>
2015-10-30 01:41:25 +0100

    Centuries can now be filtered by year and program

--- AngularRoomManagement/src/main/java/de/nak/ttmg/dao/CenturiaDAO.java
+++ AngularRoomManagement/src/main/java/de/nak/ttmg/dao/CenturiaDAO.java
@@ -5,10 +5,14 @@ import de.nak.ttmg.model.StudyProgram;
 import de.nak.ttmg.util.EntityAlreadyExistsException;
 import de.nak.ttmg.util.InvalidParameterException;
 import de.nak.ttmg.util.ValidationException;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.exception.ConstraintViolationException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
 /**
@@ -19,9 +23,15 @@ public class CenturiaDAO {
 
     @SuppressWarnings({"unchecked", "JpaQlInspection"})
     public List<Centuria> findAll(Integer year, StudyProgram program) {
-        //TODO
-        return entityManager.createQuery("select centuria from Centuria centuria").getResultList();
-
+        Session session = entityManager.unwrap(Session.class);
+        Criteria criteria = session.createCriteria(Centuria.class);
+        if (year != null) {
+            criteria.add(Restrictions.eq("year", year));
+        }
+        if (program != null) {
+            criteria.add(Restrictions.eq("program", program));
+        }
+        return criteria.list();
     }
 
     public Centuria load(Long id) {
