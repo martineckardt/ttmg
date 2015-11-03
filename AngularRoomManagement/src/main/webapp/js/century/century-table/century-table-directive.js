d4f3bb75c66c6c86946e912bfba6c587a1b28001
martineckardt <m.eckardt@outlook.com>
2015-11-03 22:52:21 +0100

    Added Driectives for room-table and centurias-table

--- AngularRoomManagement/src/main/webapp/js/century/century-table/century-table-directive.js
+++ AngularRoomManagement/src/main/webapp/js/century/century-table/century-table-directive.js
@@ -1,6 +1,8 @@
 /**
  * Created by U519643 on 03.11.2015.
+ * Directive for displaying a set of centurias in a table.
  */
+
 angular.module('ttmg').directive('centuryTable', function () {
     return {
         restrict: 'E',
