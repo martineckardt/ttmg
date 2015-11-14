6a9fe4d7f743564f74ecf58eb014de618a055111
martineckardt <m.eckardt@outlook.com>
2015-11-14 17:43:49 +0100

    Added room enumeration directive to display rooms as comma separated

---
+++ AngularRoomManagement/src/main/webapp/js/room/room-enumeration/room-enumeration-directive.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Martin Eckardt on 14.11.2015.
+ * Directive to show rooms as a comma-separated String
+ */
+
+angular.module('ttmg').directive('roomEnumeration', function () {
+    return {
+        restrict: 'E',
+        scope: {rooms: '='},
+        templateUrl: 'js/room/room-enumeration/room-enumeration-template.html'
+    };
+});
