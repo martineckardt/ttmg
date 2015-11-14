9bb5f49fda5e7a23377510da2d863d4dbcd4367b
martineckardt <m.eckardt@outlook.com>
2015-11-14 15:10:46 +0100

    Update and standarized factory comments

--- AngularRoomManagement/src/main/webapp/js/room/room-factory.js
+++ AngularRoomManagement/src/main/webapp/js/room/room-factory.js
@@ -1,6 +1,6 @@
 /**
  * Created by Martin Eckardt on 28.10.2015.
- * Factory for querying all rooms and creating new rooms.
+ * Factory for querying a specific entitiy, all entities and creating a new entity of the type room.
  */
 
 angular.module('ttmg.services').factory('RoomFactory', function ($resource) {
