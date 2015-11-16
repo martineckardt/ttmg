5b8d68405c3b587179cc34424cc8bda099dcdc49
martineckardt <m.eckardt@outlook.com>
2015-11-16 15:47:57 +0100

    create course date selection, room selection

--- AngularRoomManagement/src/main/webapp/js/course/create-course/create-course-controller.js
+++ AngularRoomManagement/src/main/webapp/js/course/create-course/create-course-controller.js
@@ -1,8 +1,4 @@
-/**
- * Created by Martin Eckardt on 16.11.2015.
- */
-
-angular.module('ttmg.controllers').controller('createCourseController', ['$scope', 'CourseFactory', 'CenturiaFactory', 'TutorFactory', function ($scope, CourseFactory, CenturiaFactory, TutorFactory) {
+angular.module('ttmg.controllers').controller('createCourseController', ['$scope', 'CourseFactory', 'CenturiaFactory', 'TutorFactory', 'RoomFactory', function ($scope, CourseFactory, CenturiaFactory, TutorFactory, RoomFactory) {
     console.log('createCourseController initialized');
 
     $scope.formState = 1;
@@ -11,14 +7,25 @@ angular.module('ttmg.controllers').controller('createCourseController', ['$scope
     $scope.model = {
         course: new CourseFactory(),
         tutors: TutorFactory.query(),
-        centurias: CenturiaFactory.query()
+        centurias: CenturiaFactory.query(),
+        eventForDateSelection: {
+            begin: new Date(),
+            end: new Date(),
+            repeatForWeeks: 1
+        },
+        eventsForRoomSelection: []
     };
 
+    var course = $scope.model.course;
+    var eventForDateSelection = $scope.model.eventForDateSelection;
+    var eventsForRoomSelection = $scope.model.eventsForRoomSelection;
+
     this.proceedToEventSelection = function () {
-        var course = $scope.model.course;
+        // Reset values
+        course.participants = {};
         var numberOfParticipants = 0;
 
-        if (course.type != 'SEMINAR') {
+        if (course.type != 'SEMINAR') { // if the course is not a seminar
             // Add selected centruias to course
             course.participants = $scope.model.centurias.filter(function filter(participant) {
                 return participant.selected == true;
@@ -29,21 +36,62 @@ angular.module('ttmg.controllers').controller('createCourseController', ['$scope
                 numberOfParticipants += currentParticipant.nbrOfStudents;
             });
         }
-        else { // The course is a seminar
-            // Delete all previously selected centurias
-            course.participants = {};
-        }
 
         $scope.model.numberOfParticipants = numberOfParticipants;
         $scope.formState = 2;
+    };
 
-        $scope.model.course.events = {
-            1: {
-                begin: new Date(),
-                end: new Date()
-            }
+    this.proceedToRoomSelection = function () {
+        // Reset room selection
+        eventsForRoomSelection.splice(0, eventsForRoomSelection.length);
+
+        // create events
+        for (repetitionIndex = 0; repetitionIndex < eventForDateSelection.repeatForWeeks; repetitionIndex++) {
+            var currentBegin = new Date();
+            var currentEnd = new Date();
+            // Query available rooms for this event using the number of participants
+            var currentAvailableRooms = RoomFactory.query({
+                //freeStart: currentBegin.getTime(),
+                //freeEnd: currentEnd.getTime(),
+                minSeats: $scope.model.numberOfParticipants
+            });
+
+            var currentEvent = {
+                begin: currentBegin,
+                end: currentEnd,
+                availableRooms: currentAvailableRooms
+            };
+            eventsForRoomSelection.push(currentEvent);
         }
+        $scope.formState = 3;
+    };
 
+    this.addCourse = function () {
+        var courseEvents = [];
+        eventsForRoomSelection.forEach(function (currentEvent) {
+            // Filter the selected rooms
+            var selectedRoomsForCurrentEvent = currentEvent.availableRooms.filter(function selectedFilter(room) {
+                return room.selected == true;
+            });
+
+            var event = {
+                begin: currentEvent.begin,
+                end: currentEvent.end,
+                rooms: selectedRoomsForCurrentEvent
+            };
+            courseEvents.push(event);
+        });
+
+        $scope.model.course.events = courseEvents;
+        $scope.formState = 4;
+    };
+
+    this.goBackToStep = function (stepId) {
+        $scope.formState = stepId;
     };
 
 }]);
+
+/**
+ * Created by Martin Eckardt on 16.11.2015.
+ */
