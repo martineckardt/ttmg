995058a5e8d51f03607f6b59937293a5422e1f63
martineckardt <m.eckardt@outlook.com>
2015-11-18 15:45:55 +0100

    Added entityName and fixed Type

--- AngularRoomManagement/src/main/webapp/js/common/delete-entity-model-directive/delete-entity-modal-directive.js
+++ AngularRoomManagement/src/main/webapp/js/common/delete-entity-model-directive/delete-entity-modal-directive.js
@@ -1,5 +1,10 @@
 /**
  * Created by Martin Eckardt on 18.11.2015.
+ * Directive that shows a delete-button for an entity. This button opens a modal that asks the user to confirm his wish to delete the entity or cancel the action.
+ *
+ * If the user confirms the controller tries to delete the entity. On success it switches to the specifies success view. If the deletion fails the message of the backend exception is displayed.
+ *
+ * The cancel button closes the modal.
  */
 
 angular.module('ttmg').directive('deleteEntityModal', function () {
@@ -8,7 +13,8 @@ angular.module('ttmg').directive('deleteEntityModal', function () {
         scope: {
             entityRessource: '=',
             entityType: '@',
-            successHref: '@'
+            entityName: '@',
+            successView: '@'
         },
         controller: ['$scope', '$location', function ($scope, $location) {
             console.log("deleteEntityController started");
@@ -19,12 +25,15 @@ angular.module('ttmg').directive('deleteEntityModal', function () {
                         console.log("successfully deleted entity");
                         console.log(data);
 
+                        // Hide the modal
+                        $('#deleteCourseModal').modal('hide');
+
                         // Redirecting to successHref
-                        $location.path($scope.successHref);
+                        $location.path($scope.successView);
                     }, function errorCallback(error) {
                         console.log("failed to delete entity");
                         console.log(error);
-
+                        // Show error message
                         $scope.deleteEntityErrorMessage = error.data.message;
                     });
             };
